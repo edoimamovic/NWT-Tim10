3726949b5bbfdff1ef356594ff7201ee4a0d19b1
ahamza1 <ahamza1@etf.unsa.ba>
2018-03-20 20:38:19 +0100

    Added project for Users module

---
+++ modules/etfmovie-users/src/main/java/com/etfmovies/users/EtfMovieUsersApplication.java
@@ -0,0 +1,12 @@
+package com.etfmovies.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtfMovieUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtfMovieUsersApplication.class, args);
+	}
+}
